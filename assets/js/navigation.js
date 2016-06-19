e6a4e5201c2d9bba13ba827d2711ae6e6b270f51
Toby Schrapel <toby@tobyschrapel.com>
2016-06-19 20:38:40 +0100

    Changes for #3, #4. Add JavaScript to handle repositioning when menus go off-screen. Drop down toggle and link are now both clickable

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -10,11 +10,32 @@
 		$('#site-navigation').toggle();
 	})
 
-	$('.menu-item-has-children > a').click(function(e){
+	$('.menu-item-has-children > a > span').click(function(e){
 		if($(window).width() < 640) {
 			e.preventDefault();
-			$(this).parent().find( ".sub-menu" ).first().toggle();
+			$(this).parent().parent().find( ".sub-menu" ).first().toggle();
 		}
+	});
+
+
+	$('.main-navigation li.menu-item-has-children').hover(function() {
+    var menu = $(this).find("> .sub-menu");
+    var menu_position = $(menu).offset();
+
+		if ( menu_position.left + menu.width() > $(window).width() ) {
+
+			if( $(this).closest(".sub-menu").length > 0 ) {
+				var new_position = -$(menu).width();
+				menu.css({
+					left: new_position
+				});
+			} else {
+				menu.css({
+					left: -((menu_position.left + menu.width()) - $(window).width()) - 10
+				});
+			}
+
+    }
 	});
 
 })( jQuery );
--- templates/parts/primary-navigation.php
+++ templates/parts/primary-navigation.php
@@ -8,6 +8,12 @@
 
 <div class="main-navigation-container">
 	<nav id="site-navigation" class="main-navigation" role="navigation">
-		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'primary',
+				'link_after' => '<span></span>' 
+			)
+		); ?>
 	</nav><!-- #site-navigation -->
 </div>
