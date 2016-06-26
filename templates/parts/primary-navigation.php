68dd2f6635ebe728dfb001ff38b317764bce726e
Toby Schrapel <toby@tobyschrapel.com>
2016-06-26 18:20:12 +0200

    Only output wp_nav_menu() if a primary nav menu exists

--- templates/parts/primary-navigation.php
+++ templates/parts/primary-navigation.php
@@ -6,14 +6,16 @@
  */
 ?>
 
-<div class="main-navigation-container">
-	<nav id="site-navigation" class="main-navigation" role="navigation">
-		<?php
-		wp_nav_menu(
-			array(
-				'theme_location' => 'primary',
-				'link_after' => '<span></span>'
-			)
-		); ?>
-	</nav><!-- #site-navigation -->
-</div>
+<?php if ( has_nav_menu( 'primary' ) ) :  ?>
+	<div class="main-navigation-container">
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'primary',
+					'link_after' => '<span></span>',
+				)
+			); ?>
+		</nav><!-- #site-navigation -->
+	</div>
+<?php endif; ?>
