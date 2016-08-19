e2ab21bb1dd8ea457b599a31a6a5423468260c30
Frankie Jarrett <fjarrett@godaddy.com>
2016-08-19 15:07:36 -0500

    Add site title and navigation font support

--- functions.php
+++ functions.php
@@ -8,16 +8,12 @@
  */
 function ascension_move_elements() {
 
-	// Hero image
-	remove_action( 'primer_header', 'primer_add_hero' );
-	add_action( 'primer_after_header', 'primer_add_hero' );
-
-	// Primary navigation
+	remove_action( 'primer_header',       'primer_add_hero' );
 	remove_action( 'primer_after_header', 'primer_add_primary_navigation' );
-	add_action( 'primer_header', 'primer_add_primary_navigation' );
-
-	// Page titles
 	remove_action( 'primer_after_header', 'primer_add_page_title' );
+
+	add_action( 'primer_after_header', 'primer_add_hero' );
+	add_action( 'primer_header',       'primer_add_primary_navigation' );
 
 	if ( ! is_front_page() ) {
 
@@ -75,7 +71,7 @@ add_filter( 'primer_custom_logo_args', 'ascension_custom_logo_args' );
  */
 function ascension_default_hero_images( $defaults ) {
 
-	$defaults['default']['description'] = esc_html__( 'Professional Woman', 'ascension' );
+	$defaults['default']['description'] = esc_html__( 'Professional woman reading a tablet', 'ascension' );
 
 	return $defaults;
 
@@ -83,7 +79,7 @@ function ascension_default_hero_images( $defaults ) {
 add_filter( 'primer_default_hero_images', 'ascension_default_hero_images' );
 
 /**
- * Register sidebar areas.
+ * Set sidebars.
  *
  * @filter primer_sidebars
  * @since  1.0.0
@@ -109,413 +105,216 @@ function ascension_sidebars( $sidebars ) {
 add_filter( 'primer_sidebars', 'ascension_sidebars' );
 
 /**
- * Register font types.
+ * Set fonts.
  *
- * @action primer_font_types
+ * @filter primer_fonts
  * @since  1.0.0
+ *
+ * @param  array $fonts
  *
  * @return array
  */
-function ascension_font_types() {
+function ascension_fonts( $fonts ) {
 
-	return array(
-		'primary_font' => array(
-			'label'   => esc_html__( 'Primary Font', 'ascension' ),
+	$fonts[] = 'Open Sans';
+
+	return $fonts;
+
+}
+add_filter( 'primer_fonts', 'ascension_fonts' );
+
+/**
+ * Set font types.
+ *
+ * @filter primer_font_types
+ * @since  1.0.0
+ *
+ * @param  array $font_types
+ *
+ * @return array
+ */
+function ascension_font_types( $font_types ) {
+
+	$overrides = array(
+		'site_title_font' => array(
 			'default' => 'Open Sans',
-			'css'     => array(
-				'body,
-				p,
-				h1, h2, h3, h4, h5, h6,
-				ul, ol, dl,
-				blockquote,
-				blockquote .aligncenter, blockquote .aligncenter cite,
-				button, a.button, .social-menu a, input, select, textarea,
-				label,
-				legend,
-				.main-navigation ul li a,
-				.widget-title,
-				.entry-footer,
-				.entry-meta,
-				.post-format,
-				.more-link,
-				article.format-link,
-				.comment-list li .comment-author, .comment-list li .comment-metadata,
-				#respond,
-				.site-title,
-				.site-description,
-				.featured-content .entry-header .entry-title,
-				.featured-content .entry-header .read-more' => array(
-					'font-family' => '"%s", sans-serif',
-				),
-			),
+		),
+		'navigation_font' => array(
+			'default' => 'Open Sans',
+		),
+		'heading_font' => array(
+			'default' => 'Open Sans',
+		),
+		'primary_font' => array(
+			'default' => 'Open Sans',
+		),
+		'secondary_font' => array(
+			'default' => 'Open Sans',
 		),
 	);
+
+	return primer_array_replace_recursive( $font_types, $overrides );
 
 }
 add_filter( 'primer_font_types', 'ascension_font_types' );
 
 /**
- * Register colors.
+ * Set colors.
  *
  * @filter primer_colors
  * @since  1.0.0
  *
+ * @param  array $colors
+ *
  * @return array
  */
-function ascension_colors() {
+function ascension_colors( $colors ) {
 
-	return array(
+	unset(
+		$colors['content_background_color'],
+		$colors['menu_background_color']
+	);
+
+	$overrides = array(
+		/**
+		 * Text colors
+		 */
 		'header_textcolor' => array(
+			'default'  => '#194f6e',
+		),
+		'tagline_text_color' => array(
+			'default'  => '#686868',
+		),
+		'menu_text_color' => array(
 			'default' => '#194f6e',
 			'css'     => array(
-				'.site-title a, .site-title a:visited' => array(
-					'color' => '%1$s',
+				'.main-navigation ul li.current-menu-item, .main-navigation ul li:hover' => array(
+					'border-color' => '%1$s',
 				),
-			),
-			'rgba_css' => array(
-				'.site-title a:hover, .site-title a:visited:hover' => array(
-					'color' => 'rgba(%1$s, 0.8)',
+				'.main-navigation ul ul, .main-navigation .sub-menu' => array(
+					'background-color' => '%1$s',
 				),
 			),
 		),
+		/**
+		 * Link / Button colors
+		 */
+		'link_color' => array(
+			'default'  => '#39baf3',
+		),
+		'button_color' => array(
+			'default'  => '#39baf3',
+		),
+		/**
+		 * Background colors
+		 */
 		'background_color' => array(
 			'default' => '#ffffff',
 			'css'     => array(
-				'body,
-				.gallery-caption,
-				.featured-content .entry-header .entry-header-column,.main-navigation a,.main-navigation.open,.main-navigation .expand:hover,.main-navigation ul ul' => array(
-					'background-color' => '%1$s',
-				),
-			),
-		),
-		'tagline_text_color' => array(
-			'default' => '#545454',
-			'css'     => array(
-				'.site-description' => array(
+				'.main-navigation ul li.menu-item-has-children .sub-menu li a' => array(
 					'color' => '%1$s',
 				),
 			),
 		),
-		'main_text_color' => array(
-			'default' => '#212121',
-			'css'     => array(
-				'body,
-				.site-footer,
-				.footer-widget-area .footer-widget .widget,
-				.main-navigation a,
-				h1,
-				h2,
-				h3,
-				section.error-404 p,
-				.widget_calendar #calendar_wrap #wp-calendar,
-				.widget_calendar #calendar_wrap #wp-calendar tfoot td a,
-				.widget_calendar #calendar_wrap #wp-calendar tbody tr td a,
-				.widget_calendar #calendar_wrap #wp-calendar caption' => array(
-					'color' => '%1$s',
-				),
-			),
-		),
-		'secondary_text_color' => array(
+		'hero_background_color' => array(
 			'default' => '#194f6e',
-			'css'     => array(
-				'h3,
-				.hentry .entry-header,
-				.entry-title a,
-				.archive-title a,
-				.site-info-wrapper .social-menu a,
-				.site-info-wrapper .social-menu a:visited,
-				.social-menu a,
-				.social-menu a:visited,
-				.widget-title' => array(
-					'color' => '%1$s',
-				),
-				'.search-form .search-field,
-				.bypostauthor,
-				.main-navigation ul li.current-menu-item a,
-                .main-navigation ul li a:hover,' => array(
-					'border-color' => '%1$s',
-				),
-				'button,
-				a.button,
-				.social-menu a,
-				.social-menu a:visited,
-				a.button:visited,
-				.social-menu a:visited,
-				input[type="button"],
-				input[type="reset"],
-				input[type="submit"],
-				.fl-builder-content a.fl-button,
-				.fl-builder-content a.fl-button:visited,
-				.search-form .search-field' => array(
-					'color'        => '%1$s',
-					'border-color' => '%1$s',
-				),
-				'.hero,
-				button:hover,
-				a.button:hover,
-				.social-menu a:hover,
-				a.button:visited:hover,
-				.social-menu a:visited:hover,
-				input[type="button"]:hover,
-				input[type="reset"]:hover,
-				input[type="submit"]:hover,
-				.fl-builder-content a.fl-button:hover,
-				.fl-builder-content a.fl-button:visited:hover,
-				button:focus button:active,
-				a.button:focus a.button:active,
-				a.button:focus .social-menu a:focus,
-				.social-menu a.button:focus a:focus,
-				a.button:focus .social-menu a:active,
-				.social-menu a.button:focus a:active,
-				a.button:visited:focus a.button:visited:active,
-				a.button:visited:focus .social-menu a:visited:focus,
-				.social-menu a.button:visited:focus a:visited:focus,
-				a.button:visited:focus .social-menu a:visited:active,
-				.social-menu a.button:visited:focus a:visited:active,
-				input[type="button"]:focus input[type="button"]:active,
-				input[type="reset"]:focus input[type="reset"]:active,
-				input[type="submit"]:focus input[type="submit"]:active,
-				.fl-builder-content a.fl-button:focus .fl-builder-content a.fl-button:active,
-				.fl-builder-content a.fl-button:visited:focus .fl-builder-content a.fl-button:visited:active,
-				.social-menu a:hover,
-				section.error-404 .search-submit:hover' => array(
-					'background-color' => '%1$s',
-				),
-			),
 		),
-		'tertiary_text_color' => array(
-			'label'   => esc_html__( 'Tertiary Text Color', 'ascension' ),
-			'default' => '#757575',
-			'css'     => array(
-				'h1 small,
-				h2 small,
-				h3 small,
-				h4 small,
-				h5 small,
-				h6 small,
-				h4,
-				h5,
-				h6,
-				.subheader,
-				.footer-widget-area .site-description,
-				.site-info-wrapper,
-				.site-info-wrapper a,
-				.site-info-wrapper .site-info-text,
-				.site-info-wrapper .site-info-text p,
-				legend,
-				.menu-toggle,
-				.widget_recent_entries .post-date,
-				.sticky .entry-title a:before,
-				.entry-footer,
-				.entry-meta,
-				section > h2,
-				.comment-list .comment-awaiting-moderation,
-				#respond .comment-notes,
-				#respond .form-allowed-tags,
-				#respond .logged-in-as,
-				input[type="text"],
-				input[type="email"],
-				input[type="url"],
-				input[type="password"],
-				input[type="search"],
-				input[type="number"],
-				textarea,
-				input[type="text"]:focus,
-				input[type="email"]:focus,
-				input[type="url"]:focus,
-				input[type="password"]:focus,
-				input[type="search"]:focus,
-				input[type="number"]:focus,
-				textarea:focus,
-				hr,
-				pre,
-				code,
-				blockquote cite,
-				blockquote cite a,
-				blockquote cite a:visited,
-				blockquote,
-				blockquote p' => array(
-					'color' => '%1$s',
-				),
-				'table th,
-				table td,
-				abbr,
-				acronym,
-				fieldset,
-				.main-navigation,
-				.main-navigation ul li,
-				.widget li,
-				section > h2,
-				.comment-list li.pingback,
-				.comment-list li.trackback,
-				.comment-list .comment-awaiting-moderation,
-				.featured-content .entry-header .entry-meta' => array(
-					'border-color' => '%1$s',
-				),
-			),
+		'footer_widget_background_color' => array(
+			'default' => '#f9f9fa',
 		),
-		'accent_color' => array(
-			'label'   => esc_html__( 'Accent Color', 'primer' ),
-			'default' => '#39baf3',
-			'css' => array(
-				'.hero a.button.large,
-				.hero .social-menu a.large,
-				.social-menu .hero a.large' => array(
-					'background-color' => '%1$s',
-				),
-			),
-			'rgba_css' => array(
-				'.hero a.button.large:hover,
-				.hero .social-menu a.large:hover,
-				.social-menu .hero a.large:hover' => array(
-					'background-color' => 'rgba(%1$s, 0.8)',
-				),
-			),
-		),
-		'light_color' => array(
-			'label'   => esc_html( 'Light Color', 'ascension' ),
+		'footer_background_color' => array(
 			'default' => '#ffffff',
-			'css' => array(
-				'.gallery-caption,
-				.featured-content .entry-header,
-				.featured-content .entry-header .entry-title,
-				.featured-content .entry-header .entry-title a,
-				featured-content .entry-header .entry-header-column,
-				.featured-content .entry-header .entry-meta,
-				.hero .hero-wrapper,
-				.hero h1,
-				.hero h1,
-				.hero h2,
-				.hero h3,
-				.hero h4,
-				.hero h5,
-				.hero h6,
-				.hero code,
-				.hero pre,
-				.hero blockquote,
-				.hero p,
-				.hero blockquote.large,
-				.hero blockquote.large p,
-				.hero a.button.large,
-				.hero .social-menu a.large,
-				.social-menu .hero a.large,
-				.footer-widget-area a.button:hover,
-				.footer-widget-area .social-menu a:hover,
-				.social-menu .footer-widget-area a:hover,
-				.featured-content,
-				.featured-content .entry-title a,
-				.featured-content article' => array(
-					'color' => '%1$s',
-				),
-				'.search-form .search-field,
-				.main-navigation,
-				.widget_calendar #calendar_wrap #wp-calendar td,
-				.widget_calendar #calendar_wrap #wp-calendar th,
-				.widget_calendar #calendar_wrap #wp-calendar caption,
-				.widget_calendar #calendar_wrap #wp-calendar thead th,
-				.widget_calendar #calendar_wrap #wp-calendar tbody tr td a,
-				input[type="text"],
-				input[type="email"],
-				input[type="url"],
-				input[type="password"],
-				input[type="search"],
-				input[type="number"],
-				textarea,' => array(
-					'background-color' => '%1$s',
-				),
-			),
-			'rgba_css' => array(
-				'' => array(
-					'color' => 'rgba(%1$s, 0.8)',
-				),
-				'.widget_calendar #calendar_wrap #wp-calendar tfoot,
-				article.format-link' => array(
-					'background-color' => 'rgba(%1$s, 0.8)',
-				),
-			),
-		),
-		'link_color' => array(
-			'label'   => esc_html__( 'Link Color', 'primer' ),
-			'default' => '#39baf3',
-			'css'     => array(
-				'a,
-				.featured-content .read-more,
-				.hero blockquote cite,
-				.hero blockquote cite a,
-				.featured-content .entry-header .post-format,
-				.post-format,
-				.entry-footer a,
-				.widget_calendar #calendar_wrap #wp-calendar tfoot td a:hover,
-				abbr,
-				acronym,
-				.entry-footer a:hover,
-				.entry-footer a:focus,
-				.entry-footer a:active' => array(
-					'color' => '%1$s',
-				),
-				'.featured-content .entry-header .read-more' => array(
-					'backgroud-color' => '%1$s',
-				),
-				'.featured-content .entry-header .read-more,
-				.more-link' => array(
-					'border-color' => '%1$s',
-				),
-			),
-			'rgba_css' => array(
-				'a:hover, a:focus, a:focus,
-				.featured-content .read-more:hover' => array(
-					'color' => 'rgba(%1$s, 0.8)',
-				),
-				'.featured-content .entry-header .read-more:hover' => array(
-					'background-color' => 'rgba(%1$s, 0.8)',
-				),
-			),
 		),
 	);
+
+	return primer_array_replace_recursive( $colors, $overrides );
 
 }
 add_filter( 'primer_colors', 'ascension_colors' );
 
 /**
- * Register color schemes.
+ * Set color schemes.
  *
  * @filter primer_color_schemes
  * @since  1.0.0
  *
+ * @param  array $color_schemes
+ *
  * @return array
  */
-function ascension_color_schemes() {
+function ascension_color_schemes( $color_schemes ) {
 
-	return array(
-		'dark' => array(
-			'label'  => esc_html__( 'Dark', 'primer' ),
+	$overrides = array(
+		'blush' => array(
 			'colors' => array(
-				'header_textcolor'     => '#ffffff',
-				'background_color'     => '#333333',
-				'tagline_text_color'   => '#919191',
-				'main_text_color'      => '#ffffff',
-				'secondary_text_color' => '#ffffff',
-				'tertiary_text_color'  => '#7f7f7f',
-				'accent_color'         => '#39baf3',
-				'light_color'          => '#ffffff',
-				'link_color'           => '#39baf3',
+				'header_textcolor'   => '#b84247',
+				'tagline_text_color' => '#686868',
+				'menu_text_color'    => '#b84247',
 			),
 		),
-		'green' => array(
-			'label'  => esc_html__( 'Green', 'ascension' ),
+		'bronze' => array(
 			'colors' => array(
-				'header_textcolor'     => '#0b560c',
-				'background_color'     => '#f4f4f4',
-				'tagline_text_color'   => '#545454',
-				'main_text_color'      => '#212121',
-				'secondary_text_color' => '#0b560c',
-				'tertiary_text_color'  => '#757575',
-				'accent_color'         => '#49a00b',
-				'light_color'          => '#ffffff',
-				'link_color'           => '#49a00b',
+				'header_textcolor'   => '#a0917d',
+				'tagline_text_color' => '#686868',
+				'menu_text_color'    => '#a0917d',
+			),
+		),
+		'canary' => array(
+			'colors' => array(
+				'header_textcolor'   => '#d2b160',
+				'tagline_text_color' => '#686868',
+				'menu_text_color'    => '#d2b160',
+			),
+		),
+		'dark' => array(
+			'colors' => array(
+				'hero_background_color' => '#333333',
+			),
+		),
+		'iguana' => array(
+			'colors' => array(
+				'header_textcolor'   => '#58ac70',
+				'tagline_text_color' => '#686868',
+				'menu_text_color'    => '#58ac70',
+			),
+		),
+		'muted' => array(
+			'colors' => array(
+				'header_textcolor'   => '#5a6175',
+				'tagline_text_color' => '#5a6175',
+			),
+		),
+		'plum' => array(
+			'colors' => array(
+				'header_textcolor'   => '#54496d',
+				'tagline_text_color' => '#686868',
+				'menu_text_color'    => '#54496d',
+			),
+		),
+		'rose' => array(
+			'colors' => array(
+				'header_textcolor'   => '#dc8582',
+				'tagline_text_color' => '#686868',
+				'menu_text_color'    => '#dc8582',
+			),
+		),
+		'tangerine' => array(
+			'colors' => array(
+				'header_textcolor'   => '#e38f47',
+				'tagline_text_color' => '#686868',
+				'menu_text_color'    => '#e38f47',
+			),
+		),
+		'turquoise' => array(
+			'colors' => array(
+				'header_textcolor'   => '#41cfaf',
+				'tagline_text_color' => '#686868',
+				'menu_text_color'    => '#41cfaf',
 			),
 		),
 	);
+
+	return primer_array_replace_recursive( $color_schemes, $overrides );
 
 }
 add_filter( 'primer_color_schemes', 'ascension_color_schemes' );
